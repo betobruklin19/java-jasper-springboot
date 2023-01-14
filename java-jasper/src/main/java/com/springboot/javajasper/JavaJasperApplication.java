5778a577fdb12d6f662fd3c3df373b665d158d57
Roberto S. Santos <robertosilva1991@outlook.com>
2023-01-14 13:44:37 -0300

    Estrutura Spring Boot para integração com o relatório

--- java-jasper/src/main/java/com/springboot/javajasper/JavaJasperApplication.java
+++ java-jasper/src/main/java/com/springboot/javajasper/JavaJasperApplication.java
@@ -1,12 +1,23 @@
 package com.springboot.javajasper;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.javajasper.services.ReportService;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
 
 @SpringBootApplication
 @RestController
@@ -21,12 +32,24 @@ public class JavaJasperApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaJasperApplication.class, args);
 	}
-
 		@GetMapping("/")
-		public ResponseEntity<Object> getReport(){
+		public ResponseEntity<Object> getHomePage() throws IOException{
+			
+			String fileDestiny = "C:/Users/Roberto/eclipse-workspace2/java-jasper-springboot/java-jasper/src/main/resources/index.html";
+
+			var htmlFile = new File(fileDestiny);
+			var path = Paths.get(htmlFile.getAbsolutePath());
+			var resource = new ByteArrayResource(Files.readAllBytes(path));
+			
+			return ResponseEntity.ok().contentType(MediaType.TEXT_HTML).body(resource);
+			
+		}
+		
+		@GetMapping("/reports")
+		public ResponseEntity<Object> getReport() throws FileNotFoundException, IOException, JRException{
 			
 			var response = reportService.getReport();
-			return ResponseEntity.status(200).body(response);
+			return ResponseEntity.ok().contentType(MediaType.APPLICATION_PDF).body(response);
 		}
 
 }
--- java-jasper/src/main/java/com/springboot/javajasper/services/ReportService.java
+++ java-jasper/src/main/java/com/springboot/javajasper/services/ReportService.java
@@ -1,21 +1,70 @@
 package com.springboot.javajasper.services;
 
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 
 import com.springboot.javajasper.repositories.ReportRepository;
 
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
 @Service
 public class ReportService {
-	
-final ReportRepository reportRepository; 
-	
+
+	final ReportRepository reportRepository;
+
 	public ReportService(ReportRepository reportRepository) {
 		this.reportRepository = reportRepository;
 	}
-	public List<Object> getReport() {
+
+	public ByteArrayResource getReport() throws JRException, IOException {
 		var resultado = reportRepository.getReport();
-		return resultado;
+
+		try {
+			File file = ResourceUtils.getFile("classpath:report.jrxml");
+			JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+			JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(resultado);
+			Map<String, Object> parameters = new HashMap<>();
+			parameters.put("id_aluno", "2");
+
+			
+			  JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,
+			  parameters, dataSource);
+			 
+				/*
+				 * JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,
+				 * parameters);
+				 */
+			 
+			String fileDestiny = "C:/Users/Roberto/eclipse-workspace2/java-jasper-springboot/java-jasper/src/main/resources/jasper.pdf";
+			JasperExportManager.exportReportToPdfFile(jasperPrint, fileDestiny);
+
+			var pdfFile = new File(fileDestiny);
+			var path = Paths.get(pdfFile.getAbsolutePath());
+			var resource = new ByteArrayResource(Files.readAllBytes(path));
+
+			return resource;
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw e;
+
+		}
+
 	}
+
 }
